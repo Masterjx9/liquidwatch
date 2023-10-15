fcc628ffa0fce07ee365c23e0a6de26b9d31c260
Jaton Justice <masterjx9@gmail.com>
2023-10-14 23:13:12 -0700

    just trying to push

--- liquidwatch.py
+++ liquidwatch.py
@@ -1,6 +1,15 @@
+import argparse
 import psutil
 import os 
 import json
+from outputs import email, logs
+import subprocess
+
+# Argument parsing
+parser = argparse.ArgumentParser(description="Monitor system load and notify based on specified output method.")
+parser.add_argument('--output', type=str, choices=['email', 'logs'], help='Output method: email or logs')
+args = parser.parse_args()
+
 # Get the 1-minute load average
 load1, _, _ = psutil.getloadavg()
 
@@ -8,9 +17,9 @@ print(f"Load Average: {load1}, {load1}, {load1}")
 
 # Check if the load exceeds the threshold
 # Use this for actual script
-if load1 > 5:
+# if load1 > 5:
 # Use this for testing
-# if True:
+if True:
     print(f"High load detected: {load1}")
 
     # Get a list of all running processes
@@ -30,22 +39,35 @@ if load1 > 5:
         cpu_percent = process.info['cpu_percent']
         exe = process.info['exe']
 
-        # Check if the process name matches any in our watchlist
-        if any(watch_item in name for watch_item in watchlist):
+
+        matched_process = next((item for item in watchlist if name in item), None)
+        if matched_process:
+            paths = matched_process[name]["paths"]
+            
             print(f"Watchlist process detected!")
             print(f"PID: {pid}, Name: {name}, CPU%: {cpu_percent}, Executable: {exe}")
-            watchlist_detected = True
-
-    if not watchlist_detected:
-        print("No processes from the watchlist detected. Top 3 CPU-consuming processes are:")
-        for process in processes[:3]:
-            pid = process.info['pid']
-            name = process.info['name']
-            cpu_percent = process.info['cpu_percent']
-            exe = process.info['exe']
+            for path in matched_process["paths"]:
+                print(f"Fetching logs from {path} for the past 1 minute:")
+                recentlogs = subprocess.check_output(f"tail -n 100 {path} | grep '$(date +'%b %d %H:%M')'", shell=True).decode('utf-8')
+                if args.output == "email":
+                    email.send_email("Alert: High load!", "High load detected on the server." + recentlogs, "admin@example.com", "alerts@example.com", "smtp.example.com", 465, "login", "password")
+                elif args.output == "logs":
+                    logs.log_to_file("High load detected!" + recentlogs)
+                else:
+                    print("No output wanted")
+                    
+        else:
+            print(f"Unwatched process causing high load detected!")
             print(f"PID: {pid}, Name: {name}, CPU%: {cpu_percent}, Executable: {exe}")
             print("Fetching logs using journalctl for the past 1 minute:")
-            os.system(f"journalctl -u {name} --since '1 minute ago'")
+            recentlogs = subprocess.check_output(f"journalctl -u {name} --since '1 minute ago'", shell=True).decode('utf-8')
+            if args.output == "email":
+                email.send_email("Alert: High load!", "High load detected on the server." + recentlogs, "admin@example.com", "alerts@example.com", "smtp.example.com", 465, "login", "password")
+            elif args.output == "logs":
+                logs.log_to_file("High load detected!" + recentlogs)
+            else:
+                print("No output wanted")
+
 
 else:
     print(f"Load is normal: {load1}")
